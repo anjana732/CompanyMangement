eeea56361d1bfc9102875374cf0b01905d81c26e
anjana732 <anjanasingh1257@gmail.com>
2023-10-20 01:57:11 +0530

    Created PHP for Admin login and registration

---
+++ AdminLogin.php
@@ -0,0 +1,33 @@
+<?php
+
+$server = "localhost";
+$user = "root";
+$pass = "";
+
+$con = mysqli_connect($server, $user, $pass);
+
+if (!$con) {
+    die("Connection to database failed due to" . mysqli_connect_error());
+}
+
+// Only run when form is submitted.
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $password = mysqli_real_escape_string($con, $_POST['password']);
+
+    $sql = "SELECT * FROM `Company`.`AdminTab` WHERE email = '$email' AND password = '$password'";
+
+    $result = $con->query($sql);
+
+    if ($result && $result->num_rows > 0) {
+        echo "Logged in successfully!";
+        // You can start a session here and set required session variables if needed.
+        header('Location: AdminDashboard.php');
+        exit;
+    } else {
+        echo "Invalid email or password!";
+    }
+
+    $con->close();
+}
+?>
---
+++ AdminReg.php
@@ -0,0 +1,34 @@
+<?php
+    $server ="localhost";
+    $user= "root";
+    $pass = "";
+
+    $con = mysqli_connect($server, $user, $pass);
+
+    if(!$con){
+        die("connection to this database failed due to".mysqli_connect_error());
+    }
+  //  echo "Connected to db"
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $age =$_POST['age'];
+    $designation = $_POST['designation'];
+    $doj = $_POST['doj'];
+    $password= $_POST['password'];
+    $address = $_POST['address'];
+    $phNum = $_POST['phNum'];
+
+    //$sql = "INSERT INTO `DepartmentManagement`.`Department` (`name`, `email`, `password`, `username`) VALUES ('$name', '$email', '$password', '$username');";
+    
+    $sql =" INSERT INTO  `Company`.`AdminTab` (`name`, `email`, `age`, `designation`, `date`, `password`, `address`, `phone`) VALUES ('$name', '$email', '$age', '$designation', '$doj', '$password', '$address', '$phNum');";
+    echo $sql;
+    if($con->query($sql)== true){
+        echo "Seccessfully inserted";
+        header('Location: AdminLogin.html');
+        exit;
+    }else{
+        echo "Error: $sql <br> $con->error";
+    }
+    $con->close();
+?>
